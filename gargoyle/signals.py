3694508072981d1c2854307b43b3f6c537623447
Jeff Pollard <jeff.pollard@gmail.com>
2012-02-17 14:23:17 -0800

    Add base fixture classes

---
+++ gargoyle/signals.py
@@ -0,0 +1,21 @@
+class Signal(object):
+
+    def __init__(self):
+        self.__callbacks = []
+
+    def connect(self, callback):
+        if not callable(callback):
+            raise ValueError("Callback argument must be callable")
+
+        self.__callbacks.append(callback)
+
+    def call(self, *args, **kwargs):
+        for callback in self.__callbacks:
+            callback(*args, **kwargs)
+
+
+switch_registered = Signal()
+switch_deregistered = Signal()
+switch_updated = Signal()
+switch_condition_added = Signal()
+switch_condition_deleted = Signal()
---
+++ tests/test_signals.py
@@ -0,0 +1,66 @@
+import unittest
+from nose.tools import *
+from gargoyle import signals
+from tests import fixture
+import mock
+
+
+class ActsLikeSignal(object):
+
+    @fixture
+    def callback(self):
+        return mock.Mock(name="callback")
+
+    @fixture
+    def signal_with_callback(self):
+        self.signal.connect(self.callback)
+        return self.signal
+
+    def test_signal_has_connect_callable(self):
+        ok_(callable(self.signal.connect))
+
+    def test_connect_raises_exceptiion_if_argument_is_not_callable(self):
+        assert_raises(ValueError, self.signal.connect, True)
+
+    def tests_callback_called_when_signal_is_called(self):
+        self.signal_with_callback.call()
+        self.callback.assert_called_once()
+
+    def test_signal_passes_args_along_to_callback(self):
+        self.signal_with_callback.call(1, 2.0, kw='args')
+        self.callback.assert_called_once_with(1, 2.0, kw='args')
+
+
+class TestSwitchRegisteredCallback(ActsLikeSignal, unittest.TestCase):
+
+    @fixture
+    def signal(self):
+        return signals.switch_registered
+
+
+class TestSwitchDeregisteredCallback(ActsLikeSignal, unittest.TestCase):
+
+    @fixture
+    def signal(self):
+        return signals.switch_deregistered
+
+
+class TestSwitchUpdatedCallback(ActsLikeSignal, unittest.TestCase):
+
+    @fixture
+    def signal(self):
+        return signals.switch_updated
+
+
+class TestSwitchConditionAddedCallback(ActsLikeSignal, unittest.TestCase):
+
+    @fixture
+    def signal(self):
+        return signals.switch_condition_added
+
+
+class TestSwitchConditionDeletedCallback(ActsLikeSignal, unittest.TestCase):
+
+    @fixture
+    def signal(self):
+        return signals.switch_condition_deleted
