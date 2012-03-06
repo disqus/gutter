bfbb9cc3e058977c9ae32316076bc106cd1c7bbd
Jeff Pollard <jeff.pollard@gmail.com>
2012-03-06 15:32:26 -0800

    Add default operators to settings

--- gargoyle/client/settings.py
+++ gargoyle/client/settings.py
@@ -1,8 +1,12 @@
 from modeldict.dict import MemoryDict
+from gargoyle.client.operators.comparable import *
+from gargoyle.client.operators.identity import *
+from gargoyle.client.operators.misc import *
 
 
 class manager(object):
     storage_engine = MemoryDict()
     autocreate = False
     inputs = []
-    operators = []
+    operators = (Equals, Between, LessThan, LessThanOrEqualTo, MoreThan,
+                 MoreThanOrEqualTo, Truthy, Enum, Percent, PercentRange)
