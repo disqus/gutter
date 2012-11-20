48b2460c718af88e8140b108d4a9acd9258ade8c
Jeff Pollard <jeff.pollard@gmail.com>
2012-11-20 13:22:47 -0800

    Change to vanilla namespace package

--- gargoyle/__init__.py
+++ gargoyle/__init__.py
@@ -1,5 +1,1 @@
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
+__import__('pkg_resources').declare_namespace(__name__)
